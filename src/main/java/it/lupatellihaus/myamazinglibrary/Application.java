efe8a74c162386dc445100fe97e8e4a66250f0e7
Matteo <matteo.lupatelli@nttdata.com>
2019-09-19 09:42:52 +0200

    Enable swagger

--- src/main/java/it/lupatellihaus/myamazinglibrary/Application.java
+++ src/main/java/it/lupatellihaus/myamazinglibrary/Application.java
@@ -2,8 +2,9 @@ package it.lupatellihaus.myamazinglibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableSwagger2
 @SpringBootApplication
 //@SpringBootApplication(scanBasePackages = {
 //        "it.lupatellihaus.myamazinglibrary.*"})
